0c1859df22b0219f162fc0d385a5820bb647e8e5
salunkesabhyata <sabhyatasalunke1234@gmail.com>
2023-07-27 17:50:14 +0530

    efve

--- server.py
+++ server.py
@@ -2,5 +2,5 @@ from flask import Flask
 app=Flask(__name__)
 @app.route("/", methods=["GET"])
 def root():
-  return "<h1>Welcome to flask app v1</h1>"
+  return "<h1>Welcome to flask app v2</h1>"
 app.run(host="0.0.0.0", port=5000, debug=True)
